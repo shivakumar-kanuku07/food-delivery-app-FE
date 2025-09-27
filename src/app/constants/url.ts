cecb809c72507b1e357fdba9a574ddbdbf0920f9
lkanuku <83686243+lkanuku@users.noreply.github.com>
2025-09-27 12:45:14 +0530

    Initial commit - frontend project

---
+++ src/app/constants/url.ts
@@ -0,0 +1,8 @@
+// export const API_URL_RL ='http://localhost:9091';
+// export const API_URL_Order ='http://localhost:9094';
+// export const API_URL_FC ='http://localhost:9092';
+// export const API_URL_UD ='http://localhost:9093';
+
+// export const K8ExternalIp = 'http://k8s-default-awsingre-3d4b7f90d4-122545836.eu-west-3.elb.amazonaws.com';
+
+export const K8ExternalIp = 'http://k8s-default-awsingre-9aa4c55379-296122890.eu-west-2.elb.amazonaws.com'
